4dc5e06cd7cfff96d69c0d6d6469f81412196ce3
Amirkian Kiani <74903450+DarkSnowJR@users.noreply.github.com>
2024-08-04 22:24:47 +0330

    [add] resume

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-// Add any interactive functionality if needed
+document
+  .getElementById('contactForm')
+  .addEventListener('submit', function (event) {
+    event.preventDefault()
+
+    emailjs
+      .send('service_r6df6y9', 'template_7afeu2h', {
+        from_name: document.getElementById('name').value,
+        from_email: document.getElementById('email').value,
+        message: document.getElementById('message').value,
+      })
+      .then(
+        function (response) {
+          console.log('SUCCESS!', response.status, response.text)
+        },
+        function (error) {
+          console.log('FAILED...', error)
+        }
+      )
+  })
